2f5e3d9eb418263d46e7cc4fe6d916a91299fcb0
dpoonia <dshpoonia@gmail.com>
2022-12-13 01:35:19 +0530

    Updates

---
+++ precache-manifest.336f20d13950666c1752be1c1158e846.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cd8d80be35ee48acee5",
+    "url": "/top-trump/static/css/main.4f0722d7.chunk.css"
+  },
+  {
+    "revision": "6cd8d80be35ee48acee5",
+    "url": "/top-trump/static/js/main.37e0ea78.chunk.js"
+  },
+  {
+    "revision": "c02cba445c2f05681fc5",
+    "url": "/top-trump/static/js/runtime~main.a09bd3c3.js"
+  },
+  {
+    "revision": "5492a950105cf66b8efe",
+    "url": "/top-trump/static/js/2.a5f4f422.chunk.js"
+  },
+  {
+    "revision": "ba166cef838f95181304c09fab3de61e",
+    "url": "/top-trump/static/media/pokemon.ba166cef.jpeg"
+  },
+  {
+    "revision": "d48a8973a98231204ed9be58c6095785",
+    "url": "/top-trump/static/media/user.d48a8973.png"
+  },
+  {
+    "revision": "4ee42d57399e66130757583d9419709a",
+    "url": "/top-trump/static/media/bot.4ee42d57.png"
+  },
+  {
+    "revision": "64df1ae7310c75c74b0d47fc026ff317",
+    "url": "/top-trump/index.html"
+  }
+];
